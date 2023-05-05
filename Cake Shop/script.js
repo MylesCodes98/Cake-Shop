f9ab817cbbef9acf21591dbb59f5c71cec81d07b
Myles <128922054+MylesCodes98@users.noreply.github.com>
2023-05-05 16:44:06 +0100

    Update script.js

--- Cake Shop/script.js
+++ Cake Shop/script.js
@@ -10,3 +10,58 @@ let search = document.querySelector('.search');
 document.querySelector('#search').onclick=() =>{
     search.classList.toggle('active');
 }
+
+
+var swiper = new Swiper(".product-row", {
+    spaceBetween: 10,
+    loop:true,
+    centeredSlides:true,
+    autoplay:{
+        delay:3000,
+        disableOnInteraction:false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+    },
+  });
+  
+  var swiper = new Swiper(".blog-row", {
+    spaceBetween: 30,
+    loop:true,
+    centeredSlides:true,
+    autoplay:{
+        delay:9500,
+        disableOnInteraction:false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation:{
+        nextE1 :".swiper-button-next",
+        prevE1 :".swiper-button-prev",
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 1,
+      },
+      1024: {
+        slidesPerView: 1,
+      },
+    },
+  });
